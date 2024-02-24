13738b729c0be52a1ac1bcbf242014b3f7a4b8b4
Mohammed Alsahli <mohammedalsahli@Mohammeds-Macbook-air.local>
2024-02-24 04:02:35 +0300

    add todo comment, I need also to speed up the process

--- maroof/write_xlsx.py
+++ maroof/write_xlsx.py
@@ -2,7 +2,7 @@ import pandas as pd
 from sotres import Store
 from config import HEADERS, PARAMS, BASE_URL
 
-
+# todo: write files in desktop
 def xlsx(end_count, start_count = 0) -> None:
     store_data = Store(url=BASE_URL, headers=HEADERS, params=PARAMS, end_count=end_count, start_count=start_count)
     data_dict_list = [data.model_dump() for data in store_data.store_data()]
